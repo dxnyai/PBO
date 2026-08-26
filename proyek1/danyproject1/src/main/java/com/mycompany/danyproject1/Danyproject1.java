59225a8ecb5080be206d0cdbaf39804f21322a5a
Dany <danycollege@students.untidar.ac.id>
2026-08-26 21:31:12 +0700

    Add files via upload

---
+++ proyek1/danyproject1/src/main/java/com/mycompany/danyproject1/Danyproject1.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.danyproject1;
+
+/**
+ *
+ * @author Asus Vivobook
+ */
+public class Danyproject1 {
+
+    public static void main(String[] args) {
+        System.out.println("Nama: Dany Akhdan Imaduddin "
+                + "\nNPM: 2505060062 "
+                + "\nAlamat: Kota Magelang "
+                + "\nNo HP: 081234567890");
+    }
+}
